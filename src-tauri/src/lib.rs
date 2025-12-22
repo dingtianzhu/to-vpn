e28a354fecf588bb7153a59bf87577d9c75989c3
dingtianzhu <8303849+dingtianzhu@user.noreply.gitee.com>
2025-12-22 15:19:02 +0800

    update

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -15,6 +15,7 @@ pub fn run() {
 
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
+        .plugin(tauri_plugin_http::init()) // 必须添加这一行
         .plugin(tauri_plugin_store::Builder::new().build())
         .plugin(tauri_plugin_process::init())
         .manage(VpnState::new())
